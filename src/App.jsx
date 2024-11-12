3973f2aec8b3010a09bb77d9c74d8da4dd4a34b0
oodoabel <get2abeloodo@gmail.com>
2024-11-12 13:33:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Hero from "./components/Hero";
+import Nav from "./components/nav";
+import CustomerReviews from "./components/CustomerReviews";
+import Footer from "./components/Footer";
+import SpecialOffer from "./components/SpecialOffer";
+import Services from "./components/Services";
+import Subscribe from "./components/Subscribe";
+import SuperQuality from "./components/SuperQuality";
+import PopularProducts from "./components/PopularProducts";
+
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className='padding'>
+      <PopularProducts/>
+    </section>
+    <section className='padding'>
+      <SuperQuality/>
+    </section>    
+    <section className='padding-x py-10'>
+      <Services/>
+    </section>    
+    <section className='padding'>
+      <SpecialOffer/>
+    </section><section className='bg-pale-blue padding'>
+      <CustomerReviews/>
+    </section><section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe/>
+    </section><section className='bg-black padding-t pb-8 padding- text-white'>
+      <Footer/> 
+    </section>
+    
+
+
+
+
+    
+  </main>
+)
+
+export default App;
