89317d4f33814cffe61f638b405916eb7bb632c6
oodoabel <get2abeloodo@gmail.com>
2024-11-13 01:33:53 +0100

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ( label, iconURL) => {
+  return (
+    <button className=''>
+        {label}
+        {/* <img src="" alt="" /> */}
+    </button>
+  )
+}
+
+export default Button;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+// import { Button } from './Button';
+import { arrowRight } from '../assets/icons';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+    id='home'
+    className='w-full flex xl:flex-row justify-center flex-col max-container gap-10 min-h-screen p-2'
+    >
+      <p>Our summer Collection</p>
+      <h1> 
+        <span>The New Arrival</span> <br /> <span>Nike</span> Shoes
+      </h1>
+      <p>
+        Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
+      </p>
+      {/* <Button label="Shop now" iconURL ={arrowRight} /> */}
+    </section>
+    
+
   )
 }
 
